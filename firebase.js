630468c4edac34692518bf43d50a68670e6d779d
Lokesh Chandra Kancharla <131683873+Chandra2163085@users.noreply.github.com>
2025-09-07 20:49:53 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,16 +2,17 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyApajnowXi9pAn-Hkm_3qExxWFSqe5U2LY",
-  authDomain: "gym-management-system-c0ad7.firebaseapp.com",
-  projectId: "gym-management-system-c0ad7",
-  storageBucket: "gym-management-system-c0ad7.firebasestorage.app",
-  messagingSenderId: "849487750323",
-  appId: "1:849487750323:web:dd6e898802cbc7c7e0bcc6",
+  apiKey: process.env.VITE_FIREBASE_API_KEY,
+  authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VITE_FIREBASE_APP_ID,
   measurementId: "G-GDY69KYR15"
 };
 
 const app = firebase.initializeApp(firebaseConfig);
 
 const auth = firebase.auth();
+
 const db = firebase.firestore();
